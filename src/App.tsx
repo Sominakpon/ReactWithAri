26ba406e83a779a1efc747c76081e82e3401c689
MaCarbee <rebeccaassongba33@gmail.com>
2025-05-19 17:27:57 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      {/* <Button>Click me</Button> */}
+      <button className="bg-primary-500 hover:bg-primary-700 transition duration-500 p-4 text-xl text-white rounded cursor-pointer font-nunito">Saluuut</button>
+    </div>
+  );
+}
+
+export default App;
